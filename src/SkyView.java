a29d1f96cb3f0990d85ad5658d919bb2f997800b
ahossain2677 <43207698+ahossain2677@users.noreply.github.com>
2019-01-16 08:44:17 -0500

    Update SkyView.java

--- src/SkyView.java
+++ src/SkyView.java
@@ -2,10 +2,16 @@
 public class SkyView
 {
     private double [][] view;
-        
+        public view(int h,int w)
+        {
+        this.view = new view [3][4];
+        }
+    
+            
+            
     public SkyView(int numRows, int numCols, double [] scanned )
     {
-        for()
+        for(int i = 0;)
 
     }
 
